483c5e35b13ccfb86f41ed88b81f69e5b0ab2142
AlexOdlin <rockguitarist94alex@gmail.com>
2017-04-04 19:24:50 +0300

    Update __initi__.py

--- int_to_byte/__initi__.py
+++ int_to_byte/__initi__.py
@@ -1,2 +1,16 @@
-def hello_alex():
-    return('Hello, Alex')
+from functools import reduce
+
+def int_to_byte(int_number):
+    byte_number = list()
+    int_number = int(int_number)
+    div = 0
+    while (div != 1):
+        div, mod = divmod(int_number, 2)
+        int_number = div
+        byte_number.append(mod)
+
+    byte_number.append(1)
+    byte_number.reverse()
+    result = reduce(lambda a, b: str(a) + str(b), byte_number, '')
+
+    print(result)
