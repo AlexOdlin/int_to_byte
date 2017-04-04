70a9aa4ccd6f907ca8ece57161ad99a4f28bff00
AlexOdlin <rockguitarist94alex@gmail.com>
2017-04-04 18:10:56 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='hello_alex',
+      version='1.0',
+      description='Greeting!',
+      author='Sokolov Alexandr',
+      license= 'BMSTU',
+      packages=['hello_alex'],
+      zip_safe=False)
+
